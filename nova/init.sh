680b7e3a64dca9362f5a6b51d27fa94394536ee6
Yanwei Yao <yanwei.yao@me.com>
2017-11-24 00:14:48 +0800

    fix crudini typo

--- nova/init.sh
+++ nova/init.sh
@@ -23,6 +23,7 @@ SPICE_SERVER_PROXYCLIENT_ADDRESS=${SPICE_SERVER_PROXYCLIENT_ADDRESS:-127.0.0.1}
 
 if [ "$INIT_DB" = "true" ]; then
  /bin/sh -c    "nova-manage api_db version" nova
+ /bin/sh -c    "nova-manage api_db sync" nova
  /bin/sh -c    "nova-manage cell_v2 map_cell0" nova
  /bin/sh -c    "nova-manage cell_v2 create_cell --name cell1" nova
  /bin/sh -c    "nova-manage api_db sync" nova
@@ -33,6 +34,7 @@ fi
 
 
 
+chown nova:nova -R /etc/nova /etc/ceph 
 ln -s /var/lib/openstack/bin/nova-rootwrap /usr/bin/
 ln -s /var/lib/openstack/bin/privsep-helper /usr/bin/
 
@@ -48,9 +50,9 @@ case "$PROC" in
      uwsgi --ini /etc/uwsgi/nova-placement-api.ini
     ;;
   *)
-     crudinit --set /etc/nova/nova.conf spice "html5proxy_base_url" "$SPICE_HTML5PROXY_BASE_URL"
-     crudinit --set /etc/nova/nova.conf spice "listen" "$SPICE_LISTEN"
-     crudinit --set /etc/nova/nova.conf spice "server_proxyclient_address" "$SPICE_SERVER_PROXYCLIENT_ADDRESS"
+     crudini --set /etc/nova/nova.conf spice "html5proxy_base_url" "$SPICE_HTML5PROXY_BASE_URL"
+     crudini --set /etc/nova/nova.conf spice "listen" "$SPICE_LISTEN"
+     crudini --set /etc/nova/nova.conf spice "server_proxyclient_address" "$SPICE_SERVER_PROXYCLIENT_ADDRESS"
      /bin/sh -c "nova-$PROC" nova
     ;;
 esac
